b4c1a8a6bc94f12cea73ff7ea4f74394d160485c
Shriram V <shri314@yahoo.com>
2017-01-15 23:51:08 +0530

    1. Indentation fixes to support quoted strings   - Restructured the if conditions using "continue" instead of complex if-else ladder - (at the risk of a few unknown regressions)   - Remove in_here_doc flag, instead directly depend on here_string   - Remove in_ext_quote flag, instead directly depend on ext_quote_string   - Removed defer_ext_quote flag
       Now:
       func() {
                   Y1="This----------
                      this is <<HERE--
                      qu\"otes"---
                      echo------
       }
    
       Indents as:
       func() {
          Y1="This----------
                      this is <<HERE--
                      qu\"otes"
          echo
       }

--- beautify_bash.py
+++ beautify_bash.py
@@ -44,86 +44,88 @@ class BeautifyBash:
     def beautify_string(self, data, path=''):
         tab = 0
         case_stack = []
-        in_here_doc = False
-        defer_ext_quote = False
-        in_ext_quote = False
         ext_quote_string = ''
         here_string = ''
         output = []
         line = 1
         for record in re.split('\n', data):
-            record = record.rstrip()
-            stripped_record = record.strip()
-
+            test_record = stripped_record = record.strip()
+            # strip out any escaped single characters
+            test_record = re.sub(r'\\.', '', test_record)
+            # remove '#' comments
+            test_record = re.sub(r'(\A|\s)(#.*)', '', test_record, 1)
             # collapse multiple quotes between ' ... '
-            test_record = re.sub(r'\'.*?\'', '', stripped_record)
+            test_record = re.sub(r'\'.*?\'', '', test_record)
             # collapse multiple quotes between " ... "
             test_record = re.sub(r'".*?"', '', test_record)
             # collapse multiple quotes between ` ... `
             test_record = re.sub(r'`.*?`', '', test_record)
             # collapse multiple quotes between \` ... ' (weird case)
             test_record = re.sub(r'\\`.*?\'', '', test_record)
-            # strip out any escaped single characters
-            test_record = re.sub(r'\\.', '', test_record)
-            # remove '#' comments
-            test_record = re.sub(r'(\A|\s)(#.*)', '', test_record, 1)
 
-            if(in_here_doc):  # pass on with no changes
-                output.append(record)
+            if(here_string):  # pass on with no changes
                 # now test for here-doc termination string
                 if(record == here_string):
-                    in_here_doc = False
-            else:  # not in here doc
-                if(in_ext_quote):
-                    if(re.search(ext_quote_string, test_record)):
-                        # provide line after quotes
-                        test_record = re.sub('.*%s(.*)' % ext_quote_string, '\\1', test_record, 1)
-                        in_ext_quote = False
-                else:  # not in ext quote
-                    if(re.search(r'(\A|\s)(\'|")', test_record)):
-                        # apply only after this line has been processed
-                        defer_ext_quote = True
-                        ext_quote_string = re.sub('.*([\'"]).*', '\\1', test_record, 1)
-                        # provide line before quote
-                        test_record = re.sub('(.*)%s.*' % ext_quote_string, '\\1', test_record, 1)
-                if(in_ext_quote):
+                    here_string = ''
+                output.append(record)
+                continue
+
+            if(ext_quote_string):
+                if(re.search(ext_quote_string, test_record)):
+                    # provide line after quotes
+                    test_record = re.sub('.*%s(.*)' % ext_quote_string, '\\1', test_record, 1)
+                    ext_quote_string = ''
+                    # pass on left side unchanged
+                    output.append(record.rstrip())
+                else:
                     # pass on unchanged
                     output.append(record)
-                else:  # not in ext quote
-                    inc = len(re.findall('(\s|\A|;)(case|then|do)(;|\Z|\s)', test_record))
-                    inc += len(re.findall('(\{|\(|\[)', test_record))
-                    outc = len(re.findall('(\s|\A|;)(esac|fi|done|elif)(;|\)|\||\Z|\s)', test_record))
-                    outc += len(re.findall('(\}|\)|\])', test_record))
-                    if(re.search(r'\besac\b', test_record)):
-                        if(len(case_stack) == 0):
-                            sys.stderr.write('File %s: error: "esac" before "case" in line %d.\n' % (path, line))
-                        else:
-                            outc += case_stack.pop()
-                    # sepcial handling for bad syntax within case ... esac
-                    if(len(case_stack) > 0):
-                        if(re.search('\A[^(]*\)', test_record)):
-                            # avoid overcount
-                            outc -= 2
-                            case_stack[-1] += 1
-                        if(re.search(';;', test_record)):
-                            outc += 1
-                            case_stack[-1] -= 1
-                    # an ad-hoc solution for the "else" keyword
-                    else_case = (0, -1)[re.search('^(else)', test_record) != None]
-                    net = inc - outc
-                    tab += min(net, 0)
-                    extab = tab + else_case
-                    extab = max(0, extab)
-                    output.append((self.tab_str * self.tab_size * extab) + stripped_record)
-                    tab += max(net, 0)
-                if(defer_ext_quote):
-                    in_ext_quote = True
-                    defer_ext_quote = False
-                if(re.search(r'\bcase\b', test_record)):
-                    case_stack.append(0)
-                if(re.search('<<-?', test_record)):
-                    here_string = re.sub('.*<<-?\s*[\'"]?([\w]+)[\'"]?.*', '\\1', stripped_record, 1)
-                    in_here_doc = (len(here_string) > 0)
+                continue
+
+            if(re.search(r'[\'"]', test_record)):
+                # apply only after this line has been processed
+                ext_quote_string = re.sub('[^\'"]*([\'"]).*', '\\1', test_record, 1)
+                # provide line before quote
+                test_record = re.sub('(.*)%s.*' % ext_quote_string, '\\1', test_record, 1)
+                stripped_record = record.lstrip();
+
+            inc = len(re.findall('(\s|\A|;)(case|then|do)(;|\Z|\s)', test_record))
+            inc += len(re.findall('(\{|\(|\[)', test_record))
+            outc = len(re.findall('(\s|\A|;)(esac|fi|done|elif)(;|\)|\||\Z|\s)', test_record))
+            outc += len(re.findall('(\}|\)|\])', test_record))
+
+            if(re.search(r'\besac\b', test_record)):
+                if(len(case_stack) == 0):
+                    sys.stderr.write('File %s: error: "esac" before "case" in line %d.\n' % (path, line))
+                else:
+                    outc += case_stack.pop()
+
+            # sepcial handling for bad syntax within case ... esac
+            if(len(case_stack) > 0):
+                if(re.search('\A[^(]*\)', test_record)):
+                    # avoid overcount
+                    outc -= 2
+                    case_stack[-1] += 1
+                if(re.search(';;', test_record)):
+                    outc += 1
+                    case_stack[-1] -= 1
+
+            # an ad-hoc solution for the "else" keyword
+            else_case = (0, -1)[re.search('^(else)', test_record) != None]
+
+            net = inc - outc
+            tab += min(net, 0)
+            extab = tab + else_case
+            extab = max(0, extab)
+            tab += max(net, 0)
+
+            output.append((self.tab_str * self.tab_size * extab) + stripped_record);
+
+            if(re.search(r'\bcase\b', test_record)):
+                case_stack.append(0)
+            if(re.search('<<-?', test_record)):
+                here_string = re.sub('.*<<-?\s*[\'"]?([\w]+)[\'"]?.*', '\\1', record.strip(), 1)
+
             line += 1
         error = (tab != 0)
         if(error):
