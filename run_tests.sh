94989b31cc0283c90f98d1aec4f7c97f46360e06
Shriram V <shri314@yahoo.com>
2017-01-15 23:49:40 +0530

    Pretty printing of tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,8 @@
 
 ALL_TESTS=( $(echo ./testcases/*.sh) )
 TOTAL_TESTS=${#ALL_TESTS[@]};
+PASSED=0
+FAILED=0
 
 for (( i = 0 ; i < $TOTAL_TESTS; ++i ))
 do
@@ -14,20 +16,28 @@ do
       source $test_file
 
       echo "============================================================="
-      echo "TEST $test_name ($i of $TOTAL_TESTS): $(describe)"
+      echo "TEST $test_name ($((i+1)) of $TOTAL_TESTS): $(describe)"
+      echo
 
+      chr=' '
       e=0;
       for (( cc = 1 ; cc <= $CASES ; ++cc ))
       do
-         echo "-------------------------------------------------------------"
-         input$cc
-         echo "-------------------------------------------------------------"
+         echo "   CASE $cc of $CASES:"
+         echo "   -------------------------------------------------------------"
+         echo "   inp:"
+         input$cc | tee /tmp/input.txt | sed -e 's/^/   /' | sed -e "s/ /$chr/g"
+         echo "   -------------------------------------------------------------"
+
          (
             e=0;
-            diff "$@" <(input$cc | python ./beautify_bash.py -t3 - ) <(expected$cc) || e=1
+            echo "   got:                                                            exp:"
+            diff -y <(input$cc | python ./beautify_bash.py -t3 - | sed -e "s/ /$chr/g" | sed -e 's/^/   /') <(expected$cc | sed -e "s/ /$chr/g" | sed -e 's/^/   /') || e=1
 
             status=PASS && [ $e -ne 0 ] && status=FAIL
+            echo "   -------------------------------------------------------------"
             echo "   CASE $cc of $CASES - $status"
+            echo
             exit $e;
          ) || ((++e))
       done
@@ -35,8 +45,15 @@ do
       exit $e;
    ) || ((++e))
 
+   [ $e -eq 0 ] && (( PASSED++ ))
+   [ $e -ne 0 ] && (( FAILED++ ))
+
    status=PASS && [ $e -ne 0 ] && status=FAIL
    echo
-   echo "TEST $test_name ($i of $TOTAL_TESTS): $status"
+   echo "TEST $test_name ($((i+1)) of $TOTAL_TESTS): $status"
    echo "============================================================="
 done
+
+echo "============================================================="
+echo "TEST SUMMARY : PASSED: $PASSED, FAILED: $FAILED"
+echo "============================================================="
